3ebf2ae7a08347c3a783eb3647502b2c405cb440
Bryne Lee <bryne_lxd@sina.com>
2023-04-09 14:17:19 +0800

    added skywalking configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import agent from 'skywalking-backend-js';
 
 async function bootstrap() {
+
+/*  agent.start({
+    serviceName: 'fsdemo-articlenodejs',
+    serviceInstance: 'fsdemo-articlenodejs-instance',
+    collectorAddress: 'skywalking-skywalking-helm-oap:11800'
+  });*/
+
+  //skywalking agent starting using ENV variables for configuration
+  //using ENV instead of coding to suit for k8s deployment
+  agent.start();
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
